35914ba384083eeb0ec1eaab0edbcd89e4b57694
nalbam <me@nalbam.com>
2018-03-02 01:40:53 +0900

    route_table

--- main.tf
+++ main.tf
@@ -63,10 +63,6 @@ resource "aws_route_table" "public" {
 resource "aws_route_table" "private" {
   vpc_id = "${aws_vpc.default.id}"
 
-  route {
-    cidr_block = "0.0.0.0/0"
-  }
-
   tags {
     Name = "${var.prefix}-private"
   }
